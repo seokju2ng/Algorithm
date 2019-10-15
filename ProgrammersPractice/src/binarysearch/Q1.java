301fbd3f53e3f49536553f23be650eb5019a0698
seokju2ng <ehddnwnd@naver.com>
2019-10-16 03:41:16 +0900

    Solved Efficiency problem
    - 총합을 나타내는 변수 sum의 타입을 long으로 선언

--- ProgrammersPractice/src/binarysearch/Q1.java
+++ ProgrammersPractice/src/binarysearch/Q1.java
@@ -1,12 +1,20 @@
+/* **
+    User: Seokjung Kim.
+    Date: 2019.10.16.
+    Time: 3:36 AM
+    ProgrammersPractice/Q1
+    이분탐색 > 예산
+    https://programmers.co.kr/learn/courses/30/lessons/43237
+** */
 package binarysearch;
 
 public class Q1 {
     public int solution(int[] budgets, int M) {
         int answer = 0;
-        int sum = 0;
+        long sum = 0;   // 효울성 2번을 위해 필수
         int right = 0;
         int left = 0;
-        int mid = 0;
+        int mid;
 
         for(int budget : budgets) {
             sum += budget;
@@ -18,35 +26,24 @@ public class Q1 {
             return right;
         }
 
-        while(true) {
+        while(right >= left) {
             sum = 0;
             mid = (right + left) / 2;
 
             for(int budget : budgets) {
-                if(budget > mid) {
-                    sum += mid;
-                } else {
-                    sum += budget;
-                }
+                sum += budget > mid ? mid : budget;
             }
-            System.out.println("left:"+left+" / mid:"+mid+" / right:"+right+" / sum:"+sum);
 
             if (sum > M) {
                 right = mid - 1;
-            } else if (sum < M) {
+            }
+            else {
+                left = mid + 1;
                 if(answer < mid) {
                     answer = mid;
                 }
-                left = mid + 1;
-            } else {
-                return mid;
-            }
-            if(left > right){
-                break;
             }
         }
-
-
         return answer;
     }
 }
