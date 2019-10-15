3343e286a4050734b29a37b40132d70bb346762e
seokju2ng <ehddnwnd@naver.com>
2019-10-16 04:46:14 +0900

    Add Binary Search problem Q2(not yet)

--- ProgrammersPractice/src/binarysearch/Main.java
+++ ProgrammersPractice/src/binarysearch/Main.java
@@ -3,11 +3,19 @@ package binarysearch;
 public class Main {
     public static void main(String[] args) {
         // Q1
+        System.out.println("* Q1");
         int[][] budgets = {{120, 110, 140, 150}, {2, 3, 4, 5, 8, 3}, {11, 15, 23, 4, 35}, {1, 1, 4, 1, 5, 8, 1},
                 {99999, 98075, 54648, 40653, 98456, 75644, 65135, 65408, 97564, 98751}};
         int[] M = {485, 12, 100, 12, 665443};
         for (int i = 0; i < M.length; i++) {
-            System.out.println(new Q1().solution(budgets[i], M[i]));
+            System.out.println(" - "+new Q1().solution(budgets[i], M[i]));
         }   // return 127, 2, 35, 2
+
+        // Q2
+        System.out.println("\n* Q2");
+        int[] n = {6, 3, 2, 4, 6, 100, 8, 10};
+        int[][] times = {{7, 10}, {5, 2, 3, 7}, {5, 2, 3, 7}, {5, 2, 3, 7}, {5, 2, 3, 7}, {5, 2, 3, 7}, {3541,81654,5135,518,18947,3213,654,35165,1564,351,654,1531,651}, {5,10,15,10}};
+        for (int i = 0; i < n.length; i++)
+            System.out.println(" - " + new Q2().solution(n[i], times[i])); // return 28;
     }
 }
---
+++ ProgrammersPractice/src/binarysearch/Q2.java
@@ -0,0 +1,46 @@
+/* **
+    User: Seokjung Kim. 
+    Date: 2019.10.16.
+    Time: 3:52 AM
+    ProgrammersPractice/Q2
+    이분탐색 > 입국심사
+    https://programmers.co.kr/learn/courses/30/lessons/43238
+** */
+package binarysearch;
+
+import java.util.Arrays;
+
+public class Q2 {
+    /*  틀림..    정확성 5/9     */
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        long answer = n * times[times.length - 1];
+        long right = answer;
+        long left = 0;
+        long mid;
+        long people;
+
+        while (left <= right) {
+            people = 0;
+            mid = (left + right) / 2;
+            for (int time : times) {
+                people += mid / time;
+                if(mid < time) {
+                    break;
+                }
+            }
+//            System.out.println("left:"+left+" / mid:"+mid+" / right:"+right+" / people:"+people);
+            if(people >= n) {
+                right = mid - 1;
+                if (answer > mid) {
+                    answer = mid;
+                }
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return answer;
+    }
+}
