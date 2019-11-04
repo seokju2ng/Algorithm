bf9f71522834c1d9bd0f75eca3beec3192b6dd4c
seokju2ng <ehddnwnd@naver.com>
2019-11-04 17:02:13 +0900

    Added 1 fibonacci algorithm solved

--- Baekjoon/java/src/net/acmicpc/dp/P10826.java
+++ Baekjoon/java/src/net/acmicpc/dp/P10826.java
@@ -1,26 +1,27 @@
 /* **
     User: seokjung
-    Date: 2019.11.03.
-    Time: 9:46 PM
-    java/P10870.java
-    http://www.acmicpc.net/problem/10870
-    BOJ_P10870_피보나치 수 5
+    Date: 2019.11.04.
+    Time: 4:56 PM
+    java/P10826.java
+    http://www.acmicpc.net/problem/10826
+    BOJ_P10826_피보나치 수 4
 ** */
 package net.acmicpc.dp;
 
 import java.io.*;
+import java.math.BigInteger;
 
-public class P10870 {
+public class P10826 {
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] fibo = new int[n + 1];
-        fibo[0] = 0;
-        if (n > 0) fibo[1] = 1;
+        BigInteger[] fibo = new BigInteger[n + 1];
+        fibo[0] = BigInteger.ZERO;
+        if (n > 0) fibo[1] = BigInteger.ONE;
 
         for (int i = 2; i <= n; i++) {
-            fibo[i] = fibo[i - 1] + fibo[i - 2];
+            fibo[i] = fibo[i - 1].add(fibo[i - 2]);
         }
 
         bw.write(fibo[n] + "\n");
