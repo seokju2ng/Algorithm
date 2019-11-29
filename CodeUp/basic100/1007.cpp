fedaa9a5ad5ecc8f5700f11065a7a4b08b3f10e0
seokju2ng <ehddnwnd@naver.com>
2019-11-30 01:56:39 +0900

    CodeUp 1007 solved

---
+++ CodeUp/basic100/1007.cpp
@@ -0,0 +1,10 @@
+// CodeUp/1007.cpp
+// 작성자: 김석중. 작성일자: 2019.11.30.
+// https://codeup.kr/problem.php?id=1007
+
+#include <stdio.h>
+
+int main() {
+    printf("\"C:\\Download\\hello.cpp\"");
+    return 0;
+}
