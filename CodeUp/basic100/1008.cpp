90a7a1e77c65131137c1beac230e9271d7d1ffe8
seokju2ng <ehddnwnd@naver.com>
2019-12-01 02:03:50 +0900

    CodeUp 1008 solved

---
+++ CodeUp/basic100/1008.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1008.cpp
+// 작성자: 김석중. 작성일자: 2019.12.01.
+// https://codeup.kr/problem.php?id=1008
+
+#include <stdio.h>
+
+int main() {
+    printf("\u250C\u252C\u2510\n");
+    printf("\u251C\u253C\u2524\n");
+    printf("\u2514\u2534\u2518");
+    return 0;
+}
