a3b4d57a61b2d2e6810f2eed4a36ec48b72cb966
seokju2ng <ehddnwnd@naver.com>
2019-12-03 02:05:38 +0900

    CodeUp 1011 solved

---
+++ CodeUp/basic100/1011.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1011.cpp
+// 작성자: 김석중. 작성일자: 2019.12.03.
+// https://codeup.kr/problem.php?id=1011
+
+#include <stdio.h>
+
+int main() {
+    char x;
+    scanf("%c", &x);
+    printf("%c", x);
+    return 0;
+}
