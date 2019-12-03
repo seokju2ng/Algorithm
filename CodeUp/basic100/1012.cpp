60bc8ecb105268edb2374dba79539930b12d004b
seokju2ng <ehddnwnd@naver.com>
2019-12-04 02:06:52 +0900

    CodeUp 1012 solved

---
+++ CodeUp/basic100/1012.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1012.cpp
+// 작성자: 김석중. 작성일자: 2019.12.04.
+// https://codeup.kr/problem.php?id=1012
+
+#include <stdio.h>
+
+int main() {
+    float x;
+    scanf("%f", &x);
+    printf("%f", x);
+    return 0;
+}
