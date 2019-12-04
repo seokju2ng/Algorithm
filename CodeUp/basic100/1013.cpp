2da662210b1d6258e2d5590441c7babb8ff48955
seokju2ng <ehddnwnd@naver.com>
2019-12-05 02:08:02 +0900

    CodeUp 1013 solved

---
+++ CodeUp/basic100/1013.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1013.cpp
+// 작성자: 김석중. 작성일자: 2019.12.05.
+// https://codeup.kr/problem.php?id=1013
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d %d", a, b);
+    return 0;
+}
