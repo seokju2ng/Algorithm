cea52cd440f701442f80314263469adab8af4e50
seokju2ng <ehddnwnd@naver.com>
2019-12-06 02:09:37 +0900

    CodeUp 1014 solved

---
+++ CodeUp/basic100/1014.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1014.cpp
+// 작성자: 김석중. 작성일자: 2019.12.06.
+// https://codeup.kr/problem.php?id=1014
+
+#include <stdio.h>
+
+int main() {
+    char x, y;
+    scanf("%c %c", &x, &y);
+    printf("%c %c", y, x);
+    return 0;
+}
