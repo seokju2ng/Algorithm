b22124da5a21e4fe17d95ce5b69b6d94f28fe135
seokju2ng <ehddnwnd@naver.com>
2019-12-07 02:10:59 +0900

    CodeUp 1015 solved

---
+++ CodeUp/basic100/1015.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1015.cpp
+// 작성자: 김석중. 작성일자: 2019.12.07.
+// https://codeup.kr/problem.php?id=1015
+
+#include <stdio.h>
+
+int main() {
+    float x;
+    scanf("%f", &x);
+    printf("%.2f", x);
+    return 0;
+}
