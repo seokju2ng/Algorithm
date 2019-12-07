66e20805aa1b2d8d7bcb357eb51924b93fe15c77
seokju2ng <ehddnwnd@naver.com>
2019-12-08 02:18:02 +0900

    CodeUp 1017 solved

---
+++ CodeUp/basic100/1017.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1017.cpp
+// 작성자: 김석중. 작성일자: 2019.12.08.
+// https://codeup.kr/problem.php?id=1017
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d %d %d", n, n, n);
+    return 0;
+}
