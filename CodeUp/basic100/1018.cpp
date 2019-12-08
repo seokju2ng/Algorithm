29d603f77e0af09862be47f92885cdb14f45ded9
seokju2ng <ehddnwnd@naver.com>
2019-12-09 02:19:56 +0900

    CodeUp 1018 solved

---
+++ CodeUp/basic100/1018.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1018.cpp
+// 작성자: 김석중. 작성일자: 2019.12.09.
+// https://codeup.kr/problem.php?id=1018
+
+#include <stdio.h>
+
+int main() {
+    int h, m;
+    scanf("%d:%d", &h, &m);
+    printf("%d:%d", h, m);
+    return 0;
+}
