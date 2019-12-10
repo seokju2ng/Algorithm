448e2712ce374ac702b1b4ad5907f46cb749351b
seokju2ng <ehddnwnd@naver.com>
2019-12-11 02:24:21 +0900

    CodeUp 1020 solved

---
+++ CodeUp/basic100/1020.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1020.cpp
+// 작성자: 김석중. 작성일자: 2019.12.11.
+// https://codeup.kr/problem.php?id=1020
+
+#include <stdio.h>
+
+int main() {
+    int front, back;
+    scanf("%d-%d", &front, &back);
+    printf("%d%d", front, back);
+    return 0;
+}
