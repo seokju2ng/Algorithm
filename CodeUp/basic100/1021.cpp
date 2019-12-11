b01b9b12744e90429b3ca918c40d6a0bb517e9c2
seokju2ng <ehddnwnd@naver.com>
2019-12-12 02:25:34 +0900

    CodeUp 1021 solved

---
+++ CodeUp/basic100/1021.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1021.cpp
+// 작성자: 김석중. 작성일자: 2019.12.12.
+// https://codeup.kr/problem.php?id=1021
+
+#include <stdio.h>
+
+int main() {
+    char data[51] = "";
+    scanf("%s", data);
+    printf("%s", data);
+    return 0;
+}
