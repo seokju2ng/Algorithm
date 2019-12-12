7a1e0857a56e8927571227cbdea7e2aadb26c39c
seokju2ng <ehddnwnd@naver.com>
2019-12-13 02:27:12 +0900

    CodeUp 1022 solved

---
+++ CodeUp/basic100/1022.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1022.cpp
+// 작성자: 김석중. 작성일자: 2019.12.13.
+// https://codeup.kr/problem.php?id=1022
+
+#include <stdio.h>
+
+int main() {
+    char data[2001];
+    fgets(data, 2000, stdin);
+    printf("%s", data);
+    return 0;
+}
