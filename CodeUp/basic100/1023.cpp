5a44c485f854669f0973de1bb2fd5efc7b30f612
seokju2ng <ehddnwnd@naver.com>
2019-12-14 02:29:15 +0900

    CodeUp 1023 solved

---
+++ CodeUp/basic100/1023.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1023.cpp
+// 작성자: 김석중. 작성일자: 2019.12.14.
+// https://codeup.kr/problem.php?id=1023
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d.%d", &a, &b);
+    printf("%d\n%d", a, b);
+    return 0;
+}
