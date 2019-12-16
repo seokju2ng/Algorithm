419ce0f195f2852c5e9d3d2d40eb582b63a23e83
seokju2ng <ehddnwnd@naver.com>
2019-12-17 02:38:59 +0900

    CodeUp 1026 solved

---
+++ CodeUp/basic100/1026.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1026.cpp
+// 작성자: 김석중. 작성일자: 2019.12.17.
+// https://codeup.kr/problem.php?id=1026
+
+#include <stdio.h>
+
+int main() {
+    int h, m, s;
+    scanf("%d:%d:%d", &h, &m, &s);
+    printf("%d", m);
+    return 0;
+}
