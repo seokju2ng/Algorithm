60886d7f96ef3e7bcab7055ff6bab996c50ec585
seokju2ng <ehddnwnd@naver.com>
2019-12-17 02:36:23 +0900

    CodeUp 1025 solved

---
+++ CodeUp/basic100/1025.cpp
@@ -0,0 +1,16 @@
+// CodeUp/1025.cpp
+// 작성자: 김석중. 작성일자: 2019.12.16.
+// https://codeup.kr/problem.php?id=1025
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d, e;
+    scanf("%1d%1d%1d%1d%1d", &a, &b, &c, &d, &e);
+    printf("[%d0000]\n", a);
+    printf("[%d000]\n", b);
+    printf("[%d00]\n", c);
+    printf("[%d0]\n", d);
+    printf("[%d]\n", e);
+    return 0;
+}
