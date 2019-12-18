4cfc73a0686c26ab86f74258bf5a9df05af60908
seokju2ng <ehddnwnd@naver.com>
2019-12-19 02:41:51 +0900

    CodeUp 1028 solved

---
+++ CodeUp/basic100/1028.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1028.cpp
+// 작성자: 김석중. 작성일자: 2019.12.19.
+// https://codeup.kr/problem.php?id=1028
+
+#include <stdio.h>
+
+int main() {
+    unsigned int n;
+    scanf("%u", &n);
+    printf("%u", n);
+    return 0;
+}
