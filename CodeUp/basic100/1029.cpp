01c6c29899328fe2c1d4688b6481a8a8edfc3c7e
seokju2ng <ehddnwnd@naver.com>
2019-12-20 02:44:03 +0900

    CodeUp 1029 solved

---
+++ CodeUp/basic100/1029.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1029.cpp
+// 작성자: 김석중. 작성일자: 2019.12.20.
+// https://codeup.kr/problem.php?id=1029
+
+#include <stdio.h>
+
+int main() {
+    double d;
+    scanf("%lf", &d); // double(long float) 형식으로 입력
+    printf("%.11lf", d);
+    return 0;
+}
