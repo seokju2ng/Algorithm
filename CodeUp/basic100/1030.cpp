d23c0d9d46fe5d072c3e5fffd6835936af8c4c7b
seokju2ng <ehddnwnd@naver.com>
2019-12-21 02:45:19 +0900

    CodeUp 1030 solved

---
+++ CodeUp/basic100/1030.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1030.cpp
+// 작성자: 김석중. 작성일자: 2019.12.21.
+// https://codeup.kr/problem.php?id=1030
+
+#include <stdio.h>
+
+int main() {
+    long long int n;
+    scanf("%lld", &n);
+    printf("%lld", n);
+    return 0;
+}
