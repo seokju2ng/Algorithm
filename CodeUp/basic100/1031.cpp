0ebe92bdcd22f4f07a863b66c5d3424812327199
seokju2ng <ehddnwnd@naver.com>
2019-12-22 03:29:42 +0900

    CodeUp 1031 solved

---
+++ CodeUp/basic100/1031.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1031.cpp
+// 작성자: 김석중. 작성일자: 2019.12.22.
+// https://codeup.kr/problem.php?id=1031
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%o", n);
+    return 0;
+}
