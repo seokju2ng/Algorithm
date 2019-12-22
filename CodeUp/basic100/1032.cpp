6318a930fdcc9f45ae1da40de6c9c9e3a8fba4b2
seokju2ng <ehddnwnd@naver.com>
2019-12-23 03:32:10 +0900

    CodeUp 1032 solved

---
+++ CodeUp/basic100/1032.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1032.cpp
+// 작성자: 김석중. 작성일자: 2019.12.23.
+// https://codeup.kr/problem.php?id=1032
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%x", n);
+    return 0;
+}
