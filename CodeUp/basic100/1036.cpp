360d48696478ef0db5c314f83008efe3d4b8ca59
seokju2ng <ehddnwnd@naver.com>
2019-12-27 03:59:16 +0900

    CodeUp 1036 solved

---
+++ CodeUp/basic100/1036.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1036.cpp
+// 작성자: 김석중. 작성일자: 2019.12.27.
+// https://codeup.kr/problem.php?id=1036
+
+#include <stdio.h>
+
+int main() {
+    char ch;
+    scanf("%c", &ch);
+    printf("%d", ch);
+    return 0;
+}
