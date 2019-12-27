f8db7bd895e2b6bb30e2794a13334f17ee94388c
seokju2ng <ehddnwnd@naver.com>
2019-12-28 04:00:38 +0900

    CodeUp 1037 solved

---
+++ CodeUp/basic100/1037.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1037.cpp
+// 작성자: 김석중. 작성일자: 2019.12.28.
+// https://codeup.kr/problem.php?id=1037
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%c", n);
+    return 0;
+}
