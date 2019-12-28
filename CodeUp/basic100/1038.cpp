35b8ee981cc0eada96ead0167bc9a00533785997
seokju2ng <ehddnwnd@naver.com>
2019-12-29 04:02:22 +0900

    CodeUp 1038 solved

---
+++ CodeUp/basic100/1038.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1038.cpp
+// 작성자: 김석중. 작성일자: 2019.12.29.
+// https://codeup.kr/problem.php?id=1038
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d", a+b);
+    return 0;
+}
