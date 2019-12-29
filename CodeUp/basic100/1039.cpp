014c1d560f4a9b836fb1ef2af90a5d97593abbfa
seokju2ng <ehddnwnd@naver.com>
2019-12-30 04:04:03 +0900

    CodeUp 1039 solved

---
+++ CodeUp/basic100/1039.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1039.cpp
+// 작성자: 김석중. 작성일자: 2019.12.30.
+// https://codeup.kr/problem.php?id=1039
+
+#include <stdio.h>
+
+int main() {
+    long long int a, b;
+    scanf("%lld%lld", &a, &b);
+    printf("%lld", a+b);
+    return 0;
+}
