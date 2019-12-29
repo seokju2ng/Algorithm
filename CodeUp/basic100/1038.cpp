1a9a63e4ad5efc10d85f1a06cdab02b06600df93
seokju2ng <ehddnwnd@naver.com>
2019-12-29 14:38:22 +0900

    CodeUp 1038 solved

--- CodeUp/basic100/1038.cpp
+++ CodeUp/basic100/1038.cpp
@@ -5,8 +5,8 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    scanf("%d%d", &a, &b);
-    printf("%d", a+b);
+    long long int a, b;
+    scanf("%lld%lld", &a, &b);
+    printf("%lld", a+b);
     return 0;
 }
