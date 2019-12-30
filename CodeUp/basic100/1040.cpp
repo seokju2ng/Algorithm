3d58a060cfb5e57043e6f4f84899835389dee9aa
seokju2ng <ehddnwnd@naver.com>
2019-12-31 04:05:13 +0900

    CodeUp 1040 solved

---
+++ CodeUp/basic100/1040.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1040.cpp
+// 작성자: 김석중. 작성일자: 2019.12.31.
+// https://codeup.kr/problem.php?id=1040
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    printf("%d", -a);
+    return 0;
+}
