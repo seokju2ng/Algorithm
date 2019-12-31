7ca2debeb089ee31d40afa7db06acc7861e51813
seokju2ng <ehddnwnd@naver.com>
2020-01-01 03:12:40 +0900

    CodeUp 1041 solved

---
+++ CodeUp/basic100/1041.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1041.cpp
+// 작성자: 김석중. 작성일자: 2020.01.01.
+// https://codeup.kr/problem.php?id=1041
+
+#include <stdio.h>
+
+int main() {
+    char ch;
+    scanf("%c", &ch);
+    printf("%c", ch+1);
+    return 0;
+}
