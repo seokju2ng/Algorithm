ba9ee16370c671c0f5e71abf82421f70f8a121ad
seokju2ng <ehddnwnd@naver.com>
2020-01-02 03:13:06 +0900

    CodeUp 1042 solved

---
+++ CodeUp/basic100/1042.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1042.cpp
+// 작성자: 김석중. 작성일자: 2020.01.02.
+// https://codeup.kr/problem.php?id=1042
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d", a/b);
+    return 0;
+}
