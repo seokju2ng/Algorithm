b33850ba918a93da5143fc8ffaef5bd09458050b
seokju2ng <ehddnwnd@naver.com>
2020-01-03 03:13:20 +0900

    CodeUp 1043 solved

---
+++ CodeUp/basic100/1043.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1043.cpp
+// 작성자: 김석중. 작성일자: 2020.01.03.
+// https://codeup.kr/problem.php?id=1043
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d", a%b);
+    return 0;
+}
