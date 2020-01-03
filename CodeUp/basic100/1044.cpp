8ca9eaffb4397355549e67d58a672008418ad702
seokju2ng <ehddnwnd@naver.com>
2020-01-04 03:14:43 +0900

    CodeUp 1044 solved

---
+++ CodeUp/basic100/1044.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1044.cpp
+// 작성자: 김석중. 작성일자: 2020.01.04.
+// https://codeup.kr/problem.php?id=1044
+
+#include <stdio.h>
+
+int main() {
+    long long int a;
+    scanf("%lld", &a);
+    printf("%lld", a+1);
+    return 0;
+}
