36671b509785414cefdeb5e407f01178a568d54c
seokju2ng <ehddnwnd@naver.com>
2020-01-06 03:59:53 +0900

    CodeUp 1053 ~ 1058 solved

---
+++ CodeUp/basic100/1053.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1053.cpp
+// 작성자: 김석중. 작성일자: 2020.01.06.
+// https://codeup.kr/problem.php?id=1053
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    printf("%d", !a);
+    return 0;
+}
---
+++ CodeUp/basic100/1055.cpp
@@ -0,0 +1,12 @@
+// CodeUp/1055.cpp
+// 작성자: 김석중. 작성일자: 2020.01.06.
+// https://codeup.kr/problem.php?id=1054
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d", a||b);
+    return 0;
+}
