39d723aa5c49e6ef13ec76cd42e963325f14fd0e
seokju2ng <ehddnwnd@naver.com>
2020-12-31 00:25:33 +0900

    leetcode: reverse-integer
    32bit signed integer의 범위를 넘으면 0으로 반환해야하는 예외를 놓침. 해결.

--- LeetCode/reverse-integer.js
+++ LeetCode/reverse-integer.js
@@ -1,16 +1,21 @@
 // https://leetcode.com/problems/reverse-integer/
 
+const isSignedIntegerRange = x => {
+  const minValue = -Math.pow(2, 31);
+  const maxValue = -minValue - 1;
+  return x >= minValue && x <= maxValue;
+}
+
 const reverse = x => {
   const isNegative = x < 0;
-  const str = x + "";
-  let result = isNegative ? str.substr(1) : str;
-  result = result.split("").reverse().join("");
-  result = isNegative ? "-"+result : result;
-  return Number(result);
+  const reverseNumber = String(Math.abs(x)).split("").reverse().join("");
+  const sign = isNegative ? "-" : "";
+  const result = Number(sign + reverseNumber);
+  return isSignedIntegerRange(result) ? result : 0;
 }
 
 // input
-const x = [123, -123, 120, 0];
+const x = [123, -123, 120, 0, 1534236469];
 
 x.forEach(e => {
   console.log(reverse(e));
