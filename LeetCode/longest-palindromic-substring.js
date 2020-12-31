1f69c2f09450a61ce19f1d0b59661dcc4c80ff4d
seokju2ng <ehddnwnd@naver.com>
2020-12-31 18:47:49 +0900

    leetcode: longest-palindromic-substring DFS로 변경
    시간 초과 났던 문제 dfs로 해결

--- LeetCode/longest-palindromic-substring.js
+++ LeetCode/longest-palindromic-substring.js
@@ -1,24 +1,23 @@
 // https://leetcode.com/problems/longest-palindromic-substring/
 
-String.prototype.reverse = function() {
-  return this.split("").reverse().join("");
-}
-
 const longestPalindrome = s => {
   let result = "";
 
-  const isLongestPalindrome = s => 
-    (s === s.reverse()) && (result.length < s.length);
-  
+  const isRange = (a, b) => a >= 0 && b < s.length;
+
+  const isPalindrome = (a, b) => s[a] === s[b];
+
+  const dfs = (left, right) => {
+    if (!isRange(left, right) || !isPalindrome(left, right)) return;
+
+    if (result.length <= right - left) {
+      result = s.substring(left, right + 1);
+    }
+    dfs(left - 1, right + 1);
+  }
 
   for (let i = 0; i < s.length; i++) {
-    for (let j = i + 1; j <= s.length; j++) {
-      const sub = s.substring(i, j);
-      
-      if (isLongestPalindrome(sub)) {
-        result = sub;
-      }
-    }
+    dfs(i, i);  dfs(i, i + 1);
   }
   return result;
 };
@@ -29,3 +28,6 @@ const strs = ["babad", "cbbd", "a", "ac"];
 for (const s of strs) {
   console.log(longestPalindrome(s));
 }
+
+// 1차 풀이에서 시간초과났던 입력 예제
+console.log(longestPalindrome("kyyrjtdplseovzwjkykrjwhxquwxsfsorjiumvxjhjmgeueafubtonhlerrgsgohfosqssmizcuqryqomsipovhhodpfyudtusjhonlqabhxfahfcjqxyckycstcqwxvicwkjeuboerkmjshfgiglceycmycadpnvoeaurqatesivajoqdilynbcihnidbizwkuaoegmytopzdmvvoewvhebqzskseeubnretjgnmyjwwgcooytfojeuzcuyhsznbcaiqpwcyusyyywqmmvqzvvceylnuwcbxybhqpvjumzomnabrjgcfaabqmiotlfojnyuolostmtacbwmwlqdfkbfikusuqtupdwdrjwqmuudbcvtpieiwteqbeyfyqejglmxofdjksqmzeugwvuniaxdrunyunnqpbnfbgqemvamaxuhjbyzqmhalrprhnindrkbopwbwsjeqrmyqipnqvjqzpjalqyfvaavyhytetllzupxjwozdfpmjhjlrnitnjgapzrakcqahaqetwllaaiadalmxgvpawqpgecojxfvcgxsbrldktufdrogkogbltcezflyctklpqrjymqzyzmtlssnavzcquytcskcnjzzrytsvawkavzboncxlhqfiofuohehaygxidxsofhmhzygklliovnwqbwwiiyarxtoihvjkdrzqsnmhdtdlpckuayhtfyirnhkrhbrwkdymjrjklonyggqnxhfvtkqxoicakzsxmgczpwhpkzcntkcwhkdkxvfnjbvjjoumczjyvdgkfukfuldolqnauvoyhoheoqvpwoisniv"));
